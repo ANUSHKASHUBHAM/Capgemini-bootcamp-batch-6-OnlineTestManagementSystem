b14d387a9ce21af407e1a557b6fd0c841e74e734
AlokJarvis <alok.dixit.8953141221@gmail.com>
2020-09-25 01:30:48 +0530

    commit

--- src/main/java/com/capgemini/onlinetestmanagementsystem/controller/AdminController.java
+++ src/main/java/com/capgemini/onlinetestmanagementsystem/controller/AdminController.java
@@ -8,12 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.capgemini.onlinetestmanagementsystem.entity.Admin;
 import com.capgemini.onlinetestmanagementsystem.service.IAdminService;
 
 
-
+@RequestMapping("/onlinetestmanagementsystem")
 public class AdminController {
 	
 	@Autowired
