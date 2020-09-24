1dcfff54336128d000e2b34eea7b9f4cdd42b73c
manshi555 <39092106+manshi555@users.noreply.github.com>
2020-09-24 23:24:54 +0530

    Create QuestionServiceImpl.java
    impl

---
+++ src/main/java/com/capgemini/onlinetestmanagementsystem/service/QuestionServiceImpl.java
@@ -0,0 +1,53 @@
+package com.capgemini.onlinetestmanagementsystem.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.capgemini.onlinetestmanagementsystem.dao.IQuestionDao;
+import com.capgemini.onlinetestmanagementsystem.entity.Question;
+import com.capgemini.onlinetestmanagementsystem.exception.ResourceNotFoundException;
+
+@Service
+public class QuestionServiceImpl  implements IQuestionService {
+	
+@Autowired
+IQuestionDao questionDao;
+
+	@Override
+	public List<Question> getQuestion() {
+		List<Question> question = new ArrayList<Question>();  
+		questionDao.findAll().forEach(question1 -> question.add(question1));  
+		return question;  
+		
+	}
+
+	@Override
+	public Question save(Question question) {
+		return questionDao.save(question); 
+	}
+
+	@Override
+	public Optional<Question> getQuestionBySno(int sno) {
+		return questionDao.findById(sno);
+		
+	}
+
+	@Override
+	public Optional<Question> deleteQuestion(int sno) throws ResourceNotFoundException{
+	Optional<Question> question =questionDao.findById(sno);
+	if(question!=null)
+	{
+		questionDao.deleteById(sno);
+		return question;
+		
+	}else
+	{
+		throw new ResourceNotFoundException("No question exists of thid Sno");
+
+}
+}
+}
