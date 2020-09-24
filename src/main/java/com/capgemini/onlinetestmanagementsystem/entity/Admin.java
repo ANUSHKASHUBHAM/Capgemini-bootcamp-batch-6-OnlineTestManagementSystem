d2a027dfb68297a7d76edacbdc67179a2ba99120
ANUSHKASHUBHAM <60642191+ANUSHKASHUBHAM@users.noreply.github.com>
2020-09-24 14:09:39 +0530

    updated

--- src/main/java/com/capgemini/onlinetestmanagementsystem/entity/Admin.java
+++ src/main/java/com/capgemini/onlinetestmanagementsystem/entity/Admin.java
@@ -1,11 +1,15 @@
 package com.capgemini.onlinetestmanagementsystem.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="Adminlogin")
 public class Admin {
+	@Id
+	@GeneratedValue
 	private int id;
 	private String password;
 	public Admin() {
@@ -17,11 +21,22 @@ public class Admin {
 		this.id = id;
 		this.password = password;
 	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	@Override
 	public String toString() {
 		return "Admin [id=" + id + ", password=" + password + "]";
 	}
-	
 	
 
 }
