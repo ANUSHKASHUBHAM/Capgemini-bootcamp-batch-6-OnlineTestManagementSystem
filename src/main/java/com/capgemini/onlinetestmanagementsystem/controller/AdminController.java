5efc9868c33a6a1e9db1cc22a39166041b7baf3c
manshi555 <39092106+manshi555@users.noreply.github.com>
2020-09-29 12:12:34 +0530

    Update AdminController.java
    update

--- src/main/java/com/capgemini/onlinetestmanagementsystem/controller/AdminController.java
+++ src/main/java/com/capgemini/onlinetestmanagementsystem/controller/AdminController.java
@@ -1,5 +1,8 @@
 package com.capgemini.onlinetestmanagementsystem.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,10 +10,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.capgemini.onlinetestmanagementsystem.entity.Admin;
+import com.capgemini.onlinetestmanagementsystem.entity.Question;
+import com.capgemini.onlinetestmanagementsystem.exception.ResourceNotFoundException;
 import com.capgemini.onlinetestmanagementsystem.service.IAdminService;
 
 
@@ -70,5 +76,48 @@ public class AdminController {
 		adminService.deleteAdmin(adminId);
 		return "Admin with id " + adminId + " Removed Successfully";
 	}
+	@GetMapping("/question")
+	public List<Question> getAllQuestions() {
+		return adminService.getAllQuestion();
+	}
+
+	@GetMapping("/question/{sno}")
+	public ResponseEntity<Question> getQuestionById(@PathVariable(value = "sno") int sno)
+			throws ResourceNotFoundException {
+		Question question = adminService.getQuestionBySno(sno)
+				.orElseThrow(() -> new ResourceNotFoundException("Question not found for this sno :: " + sno));
+		return ResponseEntity.ok().body(question);
+	}
+
+	@PostMapping(path="/question",consumes="application/json",produces="application/json")
+	public Question createQuestion(@RequestBody Question question) {
+		return adminService.save(question);
+	}
+
+	@PutMapping("/question/{sno}")
+	public ResponseEntity<Question> updateQuestion(@PathVariable(value = "sno") int sno,
+			@RequestBody Question questionDetails) throws ResourceNotFoundException {
+		Question question = adminService.getQuestionBySno(sno)
+				.orElseThrow(() -> new ResourceNotFoundException("Question not found for this sno :: " + sno));
+
+		question.setQuestionvalue(questionDetails.getQuestionvalue());
+		question.setQuestionmarks(questionDetails.getQuestionmarks());
+		question.setQuestiondomain(questionDetails.getQuestiondomain());
+		question.setCorrectoption(questionDetails.getCorrectoption());
+		question.setOption1(questionDetails.getOption1());
+		question.setOption2(questionDetails.getOption2());
+		question.setOption3(questionDetails.getOption3());
+		question.setOption4(questionDetails.getOption4());
+		final Question updatedQuestion = adminService.save(question);
+		return ResponseEntity.ok(updatedQuestion);
+	}
+
+	@DeleteMapping("/question/{sno}")
+	public Optional<Question> deleteQuestion(@PathVariable(value = "sno") int sno) throws ResourceNotFoundException
+	{
+		return adminService.deleteQuestion(sno);
+	}
+	
+
 
 }
