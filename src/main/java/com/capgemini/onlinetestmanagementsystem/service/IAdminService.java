ecf2ddfef0687eeca7b56168ecc58d126201dbbc
ANUSHKASHUBHAM <60642191+ANUSHKASHUBHAM@users.noreply.github.com>
2020-09-29 12:58:15 +0530

    updated

--- src/main/java/com/capgemini/onlinetestmanagementsystem/service/IAdminService.java
+++ src/main/java/com/capgemini/onlinetestmanagementsystem/service/IAdminService.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 import com.capgemini.onlinetestmanagementsystem.entity.Admin;
 import com.capgemini.onlinetestmanagementsystem.entity.Question;
 import com.capgemini.onlinetestmanagementsystem.entity.TestEntity;
-import com.capgemini.onlinetestmanagementsystem.exception.ResourceNotFoundException;
 
 public interface IAdminService {
 	Admin loginAdmin(String adminName,String adminPassword);
@@ -23,27 +22,7 @@ public interface IAdminService {
     public List<Question> getAllQuestion();
     public Optional<Question> deleteQuestion(int Sno);
 	public Question save(Question question);
-<<<<<<< HEAD
+
 	public Optional<Question> getQuestionBySno(int sno);
-	public Optional<Question> deleteQuestion(int sno) throws ResourceNotFoundException;
-	
-	
-	TestEntity addTest(TestEntity test);
-
-	TestEntity updateTest(BigInteger testId, TestEntity test);
-
-	TestEntity deleteTest(BigInteger testId);
-
-	List<TestEntity> fetchAll();
-
-	TestEntity findById(BigInteger testId);
-
-=======
-	public Optional<Question> getQuestionBySno(int Sno);
-	
->>>>>>> branch 'master' of https://github.com/ANUSHKASHUBHAM/Capgemini-bootcamp-batch-6-OnlineTestManagementSystem.git
-	
-	
-	
 	
 }
